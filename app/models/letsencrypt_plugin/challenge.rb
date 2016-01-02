b2d583196679d9dab0e90a2477d5b01b616f6c89
Iain Bryson <iain@iain-bryson.ca>
2016-01-02 14:00:18 -0800

    Add comment.

--- app/models/letsencrypt_plugin/challenge.rb
+++ app/models/letsencrypt_plugin/challenge.rb
@@ -1,4 +1,6 @@
 module LetsencryptPlugin
+  # if the project doesn't use ActiveRecord, we assume the challenge will
+  # be stored in the filesystem
   if defined?(ActiveRecord::Base) == 'constant' && ActiveRecord::Base.class == Class 
     class Challenge < ActiveRecord::Base
     end
