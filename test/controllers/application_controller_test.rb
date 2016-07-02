699c640f3a1a054e188f632c8431f696df7c64ee
Lukasz Gromanowski <lgromanowski@gmail.com>
2016-07-02 11:27:37 +0200

    Corrected deprecated functions usage after update
    Signed-off-by: Lukasz Gromanowski <lgromanowski@gmail.com>

--- test/controllers/application_controller_test.rb
+++ test/controllers/application_controller_test.rb
@@ -7,19 +7,19 @@ module LetsencryptPlugin
     end
 
     test 'if challenge request is invalid when is smaller than 128 bits' do
-      get :index, challenge: 'dG9rZW4='
+      get :index, params: { challenge: 'dG9rZW4=' }
       assert_response :bad_request
       assert_match('Challenge failed - Request has invalid length!', response.body)
     end
 
     test 'if challenge request is invalid if it is larger than 256 bytes' do
-      get :index, challenge: 'a' * 257
+      get :index, params: { challenge: 'a' * 257 }
       assert_response :bad_request
       assert_match('Challenge failed - Request has invalid length!', response.body)
     end
 
     test 'if challenge is valid' do
-      get :index, challenge: 'rpzxDjD-8xrr5I1G_JBTEToVMYgjNjfSs-XZ62tRtgs'
+      get :index, params: { challenge: 'rpzxDjD-8xrr5I1G_JBTEToVMYgjNjfSs-XZ62tRtgs' }
       assert_response :ok
     end
   end
