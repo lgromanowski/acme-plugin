3daa1c523e531fcef50b57a4cf2cce0c5a686524
Les Nightingill <codehacker@comcast.net>
2016-08-17 11:14:02 -0700

    delay configuration when config file is symlinked later

--- config/initializers/letsencrypt_plugin.rb
+++ config/initializers/letsencrypt_plugin.rb
@@ -1,3 +1,7 @@
-config = YAML.load_file(Rails.root.join('config', 'letsencrypt_plugin.yml'))
-config.merge! config.fetch(Rails.env, {})
-LetsencryptPlugin.config = config
+begin
+  config = YAML.load_file(Rails.root.join('config', 'letsencrypt_plugin.yml'))
+  config.merge! config.fetch(Rails.env, {})
+  LetsencryptPlugin.config = config
+rescue Errno::ENOENT
+  LetsencryptPlugin.config_file = Rails.root.join('config', 'letsencrypt_plugin.yml')
+end
