cc0bc7ed06b09a19f1bd0006d655e1408003d850
Jasper Lyons <jasper.lyons@gmail.com>
2016-09-07 00:24:42 +0100

    Cleanup test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,31 +1,15 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
-require 'simplecov'
-SimpleCov.start
-
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
 
+require 'codeclimate-test-reporter'
+require 'simplecov'
+
 require File.expand_path('../../test/dummy/config/environment.rb', __FILE__)
-ActiveRecord::Migrator.migrations_paths = [File.expand_path('../../test/dummy/db/migrate', __FILE__)]
-ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/reporters'
-Minitest::Reporters.use!
-
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
-Minitest.backtrace_filter = Minitest::BacktraceFilter.new
-
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path('../fixtures', __FILE__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.fixtures :all
-end
-
 require 'webmock/minitest'
+
+Minitest::Reporters.use!
+CodeClimate::TestReporter.start
+SimpleCov.start
