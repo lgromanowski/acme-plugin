2b8c1b544d501c5daa110fafdd69a076edd83045
Sukrit Sunama <sunama.sukrit@gmail.com>
2017-08-11 01:40:25 +0700

    setting

--- app/models/letsencrypt_plugin/setting.rb
+++ app/models/letsencrypt_plugin/setting.rb
@@ -1,4 +1,10 @@
 module LetsencryptPlugin
-  class Setting < ActiveRecord::Base
+  if LetsencryptPlugin.config.challenge_dir_name.blank? && defined?(ActiveRecord::Base) == 'constant' && ActiveRecord::Base.class == Class
+    class Setting < ActiveRecord::Base
+    end
+  else
+    class Setting
+      attr_accessor :private_key
+    end
   end
 end
